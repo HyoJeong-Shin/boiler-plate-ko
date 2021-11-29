2c2c1f6abfcf586ebdc5eb1575c148246dd5e945
HyoJeong-Shin <kyn05298@naver.com>
2021-11-29 13:59:06 +0900

    auth route

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,18 +8,17 @@ let auth = (req, res, next) => {
 
     // token을 decode(복호화)한 후 유저를 찾음
     User.findByToken(token, (err, user) => {
+        // 유저가 없으면 인증 no
         if(err) throw err
         if(!user) return res.json({ isAuth: false, error: true})        // user가 없다면
         
+        // 유저가 있으면 인증 okay
         // user가 있다면  request에 토큰과 유저 넣어줌 => index.js에서 유저, 토큰 정보를 가질 수 (사용할 수) 있게 됨
         req.token = token
         req.user = user
         next()      // Middleware 다음으로 넘어갈 수 있게 함
     })
-
-    // 유저가 있으면 인증 okay
-
-    // 유저가 없으면 인증 no
+    
 }
 
 module.exports = { auth }
