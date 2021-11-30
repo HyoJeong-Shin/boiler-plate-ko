5ba9f11048103bc996e014c8bb9294acf993301c
HyoJeong-Shin <kyn05298@naver.com>
2021-12-01 01:51:10 +0900

    css framework ant design

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css';      // css framework Ant Design style sheet
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
